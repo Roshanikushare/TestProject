7e0d0d507ded3505e52ba6fbf76bd7e8f7099e92
monica <monica@Monika>
2022-08-19 15:35:16 +0530

    First commit

---
+++ GitDemoV1/src/org/gitdemo/Test.java
@@ -0,0 +1,18 @@
+package org.gitdemo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String s="hungry";
+    if(s=="yes")
+    {
+    	System.out.println("eat something");
+    }
+    else
+    {
+    	System.out.println("do work");
+    }
+	}
+
+}
